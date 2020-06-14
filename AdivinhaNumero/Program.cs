f3011f837f2b2a37f52257e386c49728932a8e78
sonyjunior <antonio.rolim2@etec.sp.gov.br>
2020-06-13 23:42:45 -0300

    adicionando o projeto Adivinha Numero

---
+++ AdivinhaNumero/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace AdivinhaNumero
+    
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int numero_gerado, numero_palpite;
+            string continuar = "s";
+
+            numero_gerado = new Random().Next(1, 10);
+           
+            while (true)
+            {
+                Console.WriteLine("Informe seu palpite");
+                numero_palpite = int.Parse(Console.ReadLine());
+                if (numero_palpite == numero_gerado)
+                {
+                    Console.WriteLine("Parabéns !!! Você acertou");
+                    Console.WriteLine("Informe [N] para parar SAIR [S] para continuar");
+                    continuar = Console.ReadLine();
+                    numero_gerado = new Random().Next(1, 9);
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Você errou !  You lose");
+                }              
+           
+                if (continuar == "n" || continuar == "N")
+
+                {
+                    break;
+                }
+                else
+                   {
+                     if (numero_gerado < numero_palpite)
+                    {
+                        Console.WriteLine("Informe um número MENOR");
+                    }
+                     else
+                    {
+                        Console.WriteLine("Informe um número MAIOR");
+
+                    }
+                }
+            }
+        }
+    }
+}
